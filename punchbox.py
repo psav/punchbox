b2aa5588687600c55b3ceab5caab1172fb6acec4
Pete Savage <psavage@redhat.com>
2017-07-07 10:10:13 +0100

    All command line options available

--- punchbox.py
+++ punchbox.py
@@ -5,6 +5,9 @@ import yaml
 from collections import defaultdict
 from mido import MidiFile
 
+with open('punchbox.yaml') as f:
+    config = yaml.load(f)
+
 NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 
@@ -31,32 +34,43 @@ def note_name(val):
 
 
 @click.command(help="Run punchbox on a file")
-@click.option('--filename', default=None)
-@click.option('--output', default=None)
-def main(filename, output):
-    with open('punchbox.yaml') as f:
-        config = yaml.load(f)
-    print config
-    note_data = config['note_data']
+@click.argument('filename', default=config.get('filename'))
+@click.option('--output', default=config.get('output', 'output'))
+@click.option('--musicbox', default=config.get('default_musicbox'))
+@click.option('--marker-offset', default=config.get('marker_offset', 6))
+@click.option('--marker-offset-top', default=config.get('marker_offset_top', None))
+@click.option('--marker-offset-bottom', default=config.get('marker_offset_bottom', None))
+@click.option('--marker-size', default=config.get('marker_size', 5))
+@click.option('--margin', default=config.get('margin', 20))
+@click.option('--font-size', default=config.get('font_size', 1.0))
+@click.option('--divisor', default=config.get('divisor', 67.0))
+@click.option('--transpose-upper', default=config.get('transpose', {}).get('upper', 100))
+@click.option('--transpose-lower', default=config.get('transpose', {}).get('lower', -100))
+@click.option('--page-width', default=config.get('page', {}).get('width', 297.0))
+@click.option('--page-height', default=config.get('page', {}).get('height', 210.0))
+@click.option('--debug', default=False)
+def main(filename, output, musicbox, marker_offset, marker_offset_top, marker_offset_bottom,
+         marker_size, margin, font_size, divisor, debug,
+         transpose_upper, transpose_lower, page_width, page_height):
+    note_data = config['boxen'][musicbox].get('note_data',
+                                              [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72])
+    reverse = config['boxen'][musicbox].get('reverse', False)
+    pitch = config['boxen'][musicbox].get('pitch', 2)
 
-    if config['reverse']:
+    if reverse:
         note_data = note_data[::-1]
 
-    divisor = config['divisor']
-    pitch = config['pitch']
-    font_size = config['font_size']
-    margin = config['margin']
-    marker_offset = config['marker_offset']
-    if isinstance(marker_offset, list):
-        mark_top, mark_btm = marker_offset
-    else:
-        mark_top, mark_btm = marker_offset, marker_offset
-    marker_size = config['marker_size']
-    filename = filename or config['filename']
-    output = output or config['output']
+    divisor = float(divisor)
+    font_size = float(font_size)
+    page_width = float(page_width)
+    page_height = float(page_height)
+
+    mark_top = float(marker_offset_top or marker_offset)
+    mark_btm = float(marker_offset_bottom or marker_offset)
+
     notes = []
     with MidiFile(filename) as midi_file:
-        transpose = range(config['transpose']['lower'], config['transpose']['upper'])
+        transpose = range(transpose_lower, transpose_upper)
         best_transpose = (0, 0)
         written = False
         for trans in transpose:
@@ -84,28 +98,30 @@ def main(filename, output):
             written = True
             percen = tpos / float(tpos + tneg)
             if percen > best_transpose[1]:
-                print "Transposition Candidate Report"
-                print "Transposition: {}".format(trans)
-                print "Total Notes: {}".format(count)
-                print "Notes OK: {}".format(tpos)
-                print "Distinct Notes Missing: {}".format(len(unavail))
-                print "Total Notes Missing: {}".format(tneg)
-                print "Unavailables: {}".format(unavail)
-                print "========================================"
+                if debug:
+                    print "Transposition Candidate Report"
+                    print "Transposition: {}".format(trans)
+                    print "Total Notes: {}".format(count)
+                    print "Notes OK: {}".format(tpos)
+                    print "Distinct Notes Missing: {}".format(len(unavail))
+                    print "Total Notes Missing: {}".format(tneg)
+                    print "Unavailables: {}".format(unavail)
+                    print "========================================"
                 best_transpose = (trans, percen)
             if percen == 1:
                 print "PERFECT Transposition Found"
                 break
-    print best_transpose
+    print "TRANSPOSE: {}".format(best_transpose[0])
+    print "PERCENTAGE HIT: {}%".format(best_transpose[1] * 100)
     max_time = max([p[1] for p in notes])
     notes = sorted(notes, key=lambda p: p[1])
     max_length = max_time / divisor
     print "MAX LENGTH: {}".format(max_length)
 
     stave_width = (len(note_data) - 1) * pitch + margin
-    staves_per_page = int(math.floor((config['page']['height'] - margin) / (stave_width)))
+    staves_per_page = int(math.floor((page_height - margin) / (stave_width)))
 
-    max_stave_length = config['page']['width'] - (margin * 2)
+    max_stave_length = page_width - (margin * 2)
     print "MAX STAVE LENGTH: {}".format(max_stave_length)
     no_staves_required = int(math.ceil(max_length / max_stave_length))
     print "NO STAVES: {}".format(no_staves_required)
@@ -116,10 +132,10 @@ def main(filename, output):
     no_staves = 0
     offset = 0
     for page in range(pages):
-        print page
+        print "Writing page {}..".format(page)
 
         dwg = svgwrite.Drawing("{}{}.svg".format(output, str(page)),
-            size=(mm(config['page']['width']), mm(config['page']['height'])))
+            size=(mm(page_width), mm(page_height)))
 
         for stave in range(staves_per_page):
             max_time = ((page * staves_per_page) + stave) * max_stave_length + max_stave_length
@@ -154,7 +170,7 @@ def main(filename, output):
                     note_pos = note_data.index(note[0] + best_transpose[0])
                     fill = "black"
                 except:
-                    for i, dta in enumerate(note_data[::-1] if config['reverse'] else note_data):
+                    for i, dta in enumerate(note_data[::-1] if reverse else note_data):
                         #print note[0] + best_transpose[0], dta
                         if note[0] + best_transpose[0] > dta:
                             continue
@@ -173,6 +189,8 @@ def main(filename, output):
                          mm((note_pos * pitch) + line_offset)),
                         "1mm", fill=fill))
         dwg.save()
+        if best_transpose[1] != 1:
+            print "PERFECT TRANSPOSITION NOT FOUND!"
 
 
 if __name__ == '__main__':
