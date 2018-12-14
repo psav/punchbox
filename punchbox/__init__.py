9919f8f8b1599995bd06ff9a5fc07bcf2608b8e1
Pete Savage <psavage@redhat.com>
2018-12-14 18:17:31 +0000

    Added pre-commit

--- punchbox/__init__.py
+++ punchbox/__init__.py
@@ -1,15 +1,16 @@
+import math
+from collections import defaultdict
+
 import click
 import svgwrite
-import math
 import yaml
-from collections import defaultdict
 from mido import MidiFile
 
 
-with open('punchbox.yaml') as f:
+with open("punchbox.yaml") as f:
     config = yaml.load(f)
 
-NOTE_NAMES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+NOTE_NAMES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
 DEBUG = False
 
 
@@ -20,12 +21,20 @@ def mm(val):
 def cross(dwg, size, x, y):
     hs = size / 2.0
     dwg.add(
-        dwg.line((mm(y - hs), mm(x)), (mm(y + hs), mm(x)),
-            stroke=svgwrite.rgb(0, 0, 0, '%'), stroke_width=".1mm")
+        dwg.line(
+            (mm(y - hs), mm(x)),
+            (mm(y + hs), mm(x)),
+            stroke=svgwrite.rgb(0, 0, 0, "%"),
+            stroke_width=".1mm",
+        )
     )
     dwg.add(
-        dwg.line((mm(y), mm(x - hs)), (mm(y), mm(x + hs)),
-            stroke=svgwrite.rgb(0, 0, 0, '%'), stroke_width=".1mm")
+        dwg.line(
+            (mm(y), mm(x - hs)),
+            (mm(y), mm(x + hs)),
+            stroke=svgwrite.rgb(0, 0, 0, "%"),
+            stroke_width=".1mm",
+        )
     )
 
 
@@ -57,7 +66,7 @@ def get_notes_from_midi(filename, transpose_lower, transpose_upper, note_data, t
                         notes_use[message.note] += 1
                     track_count += 1
             if DEBUG:
-                print "Track {}: {} note_on messages processed".format(i, track_count)
+                print("Track {}: {} note_on messages processed".format(i, track_count))
 
     notes = sorted(notes, key=lambda p: p[1])
 
@@ -65,62 +74,50 @@ def get_notes_from_midi(filename, transpose_lower, transpose_upper, note_data, t
     for trans in transpose:
         avail = sum([freq for note, freq in notes_use.iteritems() if note + trans in note_data])
         unavail = {
-            note_name(note): freq for note, freq in
-            notes_use.iteritems() if note + trans not in note_data
+            note_name(note): freq
+            for note, freq in notes_use.iteritems()
+            if note + trans not in note_data
         }
         percen = avail / float(len(notes))
         if percen == 1:
             best_transpose = (trans, 1)
-            print "PERFECT TRANSPOSITION FOUND"
+            print("PERFECT TRANSPOSITION FOUND")
         elif percen > best_transpose[1]:
             if DEBUG:
-                print "Transposition Candidate Report"
-                print "Transposition: {}".format(trans)
-                print "Total Notes: {}".format(len(notes))
-                print "Notes OK: {}".format(avail)
-                print "Distinct Notes Missing: {}".format(len(unavail))
-                print "Total Notes Missing: {}".format(len(notes) - avail)
-                print "Unavailables: {}".format(unavail)
-                print "========================================"
+                print("Transposition Candidate Report")
+                print("Transposition: {}".format(trans))
+                print("Total Notes: {}".format(len(notes)))
+                print("Notes OK: {}".format(avail))
+                print("Distinct Notes Missing: {}".format(len(unavail)))
+                print("Total Notes Missing: {}".format(len(notes) - avail))
+                print("Unavailables: {}".format(unavail))
+                print("========================================")
             best_transpose = (trans, percen)
     return notes, best_transpose
 
 
-class MusicBox(object):
-    def __init__(self, config):
-        self.note_data = config.get('note_data',
-                                    [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72])
-        self.reverse = config.get('reverse', False)
-        self.pitch = config.get('pitch', 2.0)
-        self.note_collision = config.get('note_collision', 5.0)
-        if self.reverse:
-            self.note_data = self.note_data[::-1]
-
-
-@click.command(help="Run punchbox on a file")
-@click.argument('filename', default=config.get('filename'))
-@click.option('--output', default=config.get('output', 'output'))
-@click.option('--name', default=config.get('name'))
-@click.option('--musicbox', default=config.get('default_musicbox'))
-@click.option('--marker-offset', default=config.get('marker_offset', 6))
-@click.option('--marker-offset-top', default=config.get('marker_offset_top', None))
-@click.option('--marker-offset-bottom', default=config.get('marker_offset_bottom', None))
-@click.option('--marker-size', default=config.get('marker_size', 5))
-@click.option('--margin', default=config.get('margin', 20))
-@click.option('--font-size', default=config.get('font_size', 1.0))
-@click.option('--divisor', default=config.get('divisor', 67.0))
-@click.option('--transpose-upper', default=config.get('transpose', {}).get('upper', 100))
-@click.option('--transpose-lower', default=config.get('transpose', {}).get('lower', -100))
-@click.option('--page-width', default=config.get('page', {}).get('width', 297.0))
-@click.option('--page-height', default=config.get('page', {}).get('height', 210.0))
-@click.option('--debug', default=False)
-def main(filename, output, musicbox, marker_offset, marker_offset_top, marker_offset_bottom,
-         marker_size, margin, font_size, divisor, debug, name,
-         transpose_upper, transpose_lower, page_width, page_height):
+def create_staves(
+    filename,
+    output,
+    musicbox,
+    marker_offset,
+    marker_offset_top,
+    marker_offset_bottom,
+    marker_size,
+    margin,
+    font_size,
+    divisor,
+    debug,
+    name,
+    transpose_upper,
+    transpose_lower,
+    page_width,
+    page_height,
+):
     global DEBUG
     DEBUG = debug
     name = name or filename
-    music_box = MusicBox(config['boxen'][musicbox])
+    music_box = MusicBox(config["boxen"][musicbox])
 
     divisor = float(divisor)
     font_size = float(font_size)
@@ -131,7 +128,8 @@ def main(filename, output, musicbox, marker_offset, marker_offset_top, marker_of
     mark_btm = float(marker_offset_bottom or marker_offset)
 
     notes, best_transpose = get_notes_from_midi(
-        filename, transpose_lower, transpose_upper, music_box.note_data)
+        filename, transpose_lower, transpose_upper, music_box.note_data
+    )
 
     min_note_distance = {}
     for note, ntime in notes:
@@ -146,37 +144,41 @@ def main(filename, output, musicbox, marker_offset, marker_offset_top, marker_of
             min_note_distance[note][0] = ntime
 
     min_distance = min([v[1] for v in min_note_distance.values() if v[1] is not None])
-    print "MINIMUM NOTE DISTANCE: {}".format(min_distance / divisor)
+    print("MINIMUM NOTE DISTANCE: {}".format(min_distance / divisor))
     if min_distance / divisor < music_box.note_collision:
-        print ("WARNING: SOME NOTES MAY NOT PLAY!! "
-               "{}mm note distance is less than {}mm REQUIRED".format(
-                   min_distance / divisor, music_box.note_collision))
+        print(
+            "WARNING: SOME NOTES MAY NOT PLAY!! "
+            "{}mm note distance is less than {}mm REQUIRED".format(
+                min_distance / divisor, music_box.note_collision
+            )
+        )
 
-    print "TRANSPOSE: {}".format(best_transpose[0])
-    print "PERCENTAGE HIT: {}%".format(best_transpose[1] * 100)
+    print("TRANSPOSE: {}".format(best_transpose[0]))
+    print("PERCENTAGE HIT: {}%".format(best_transpose[1] * 100))
     max_time = max([p[1] for p in notes])
     notes = sorted(notes, key=lambda p: p[1])
     max_length = max_time / divisor
-    print "MAX LENGTH: {}".format(max_length)
+    print("MAX LENGTH: {}".format(max_length))
 
     stave_width = (len(music_box.note_data) - 1) * music_box.pitch + margin
     staves_per_page = int(math.floor((page_height - margin) / (stave_width)))
 
     max_stave_length = page_width - (margin * 2)
-    print "MAX STAVE LENGTH: {}".format(max_stave_length)
+    print("MAX STAVE LENGTH: {}".format(max_stave_length))
     no_staves_required = int(math.ceil(max_length / max_stave_length))
-    print "NO STAVES: {}".format(no_staves_required)
+    print("NO STAVES: {}".format(no_staves_required))
 
     pages = int(math.ceil(float(no_staves_required) / staves_per_page))
-    print "PAGES: {}".format(pages)
+    print("PAGES: {}".format(pages))
 
     no_staves = 0
     offset = 0
     for page in range(pages):
-        print "Writing page {}..".format(page)
+        print("Writing page {}..".format(page))
 
-        dwg = svgwrite.Drawing("{}{}.svg".format(output, str(page)),
-            size=(mm(page_width), mm(page_height)))
+        dwg = svgwrite.Drawing(
+            "{}{}.svg".format(output, str(page)), size=(mm(page_width), mm(page_height))
+        )
 
         for stave in range(staves_per_page):
             max_time = ((page * staves_per_page) + stave) * max_stave_length + max_stave_length
@@ -186,34 +188,51 @@ def main(filename, output, musicbox, marker_offset, marker_offset_top, marker_of
 
             line_offset = (stave * (stave_width)) + margin
             cross(dwg, marker_size, line_offset - mark_top, margin + max_stave_length)
-            cross(dwg, marker_size,
-                line_offset + stave_width - margin + mark_btm, margin + max_stave_length)
+            cross(
+                dwg,
+                marker_size,
+                line_offset + stave_width - margin + mark_btm,
+                margin + max_stave_length,
+            )
             cross(dwg, marker_size, line_offset - mark_top, margin)
             cross(dwg, marker_size, line_offset + stave_width - margin + mark_btm, margin)
-            dwg.add(dwg.text('STAVE {} - {}'.format((page * staves_per_page) + stave, name),
-                insert=(mm(margin * 2),
-                    mm(line_offset + stave_width - margin + marker_offset)),
-                fill='blue', font_size=mm(font_size))
+            dwg.add(
+                dwg.text(
+                    "STAVE {} - {}".format((page * staves_per_page) + stave, name),
+                    insert=(mm(margin * 2), mm(line_offset + stave_width - margin + marker_offset)),
+                    fill="blue",
+                    font_size=mm(font_size),
+                )
             )
             for i, note in enumerate(music_box.note_data):
                 line_x = (i * music_box.pitch) + line_offset
                 dwg.add(
-                    dwg.line((mm(margin), mm(line_x)), (mm(max_stave_length + margin), mm(line_x)),
-                        stroke=svgwrite.rgb(0, 0, 0, '%'), stroke_width=".1mm")
+                    dwg.line(
+                        (mm(margin), mm(line_x)),
+                        (mm(max_stave_length + margin), mm(line_x)),
+                        stroke=svgwrite.rgb(0, 0, 0, "%"),
+                        stroke_width=".1mm",
+                    )
                 )
-                dwg.add(dwg.text(
-                    note_name(note), insert=(mm(-2 + margin), mm(line_x + font_size / 2)),
-                    fill='red', font_size=mm(font_size)))
+                dwg.add(
+                    dwg.text(
+                        note_name(note),
+                        insert=(mm(-2 + margin), mm(line_x + font_size / 2)),
+                        fill="red",
+                        font_size=mm(font_size),
+                    )
+                )
 
             for note in notes[offset:]:
                 offset += 1
                 try:
                     note_pos = music_box.note_data.index(note[0] + best_transpose[0])
                     fill = "black"
-                except:
+                except Exception as e:
+                    print(e)
                     for i, dta in enumerate(
-                            music_box.note_data[::-1] if
-                            music_box.reverse else music_box.note_data):
+                        music_box.note_data[::-1] if music_box.reverse else music_box.note_data
+                    ):
                         if note[0] + best_transpose[0] > dta:
                             continue
                         else:
@@ -227,13 +246,83 @@ def main(filename, output, musicbox, marker_offset, marker_offset_top, marker_of
                     break
                 dwg.add(
                     dwg.circle(
-                        (mm(note_time + margin),
-                         mm((note_pos * music_box.pitch) + line_offset)),
-                        "1mm", fill=fill))
+                        (mm(note_time + margin), mm((note_pos * music_box.pitch) + line_offset)),
+                        "1mm",
+                        fill=fill,
+                    )
+                )
         dwg.save()
         if best_transpose[1] != 1:
-            print "PERFECT TRANSPOSITION NOT FOUND!"
+            print("PERFECT TRANSPOSITION NOT FOUND!")
 
 
-if __name__ == '__main__':
+class MusicBox(object):
+    def __init__(self, config):
+        self.note_data = config.get(
+            "note_data", [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72]
+        )
+        self.reverse = config.get("reverse", False)
+        self.pitch = config.get("pitch", 2.0)
+        self.note_collision = config.get("note_collision", 5.0)
+        if self.reverse:
+            self.note_data = self.note_data[::-1]
+
+
+@click.command(help="Run punchbox on a file")
+@click.argument("filename", default=config.get("filename"))
+@click.option("--output", default=config.get("output", "output"))
+@click.option("--name", default=config.get("name"))
+@click.option("--musicbox", default=config.get("default_musicbox"))
+@click.option("--marker-offset", default=config.get("marker_offset", 6))
+@click.option("--marker-offset-top", default=config.get("marker_offset_top", None))
+@click.option("--marker-offset-bottom", default=config.get("marker_offset_bottom", None))
+@click.option("--marker-size", default=config.get("marker_size", 5))
+@click.option("--margin", default=config.get("margin", 20))
+@click.option("--font-size", default=config.get("font_size", 1.0))
+@click.option("--divisor", default=config.get("divisor", 67.0))
+@click.option("--transpose-upper", default=config.get("transpose", {}).get("upper", 100))
+@click.option("--transpose-lower", default=config.get("transpose", {}).get("lower", -100))
+@click.option("--page-width", default=config.get("page", {}).get("width", 297.0))
+@click.option("--page-height", default=config.get("page", {}).get("height", 210.0))
+@click.option("--debug", default=False)
+def main(
+    filename,
+    output,
+    musicbox,
+    marker_offset,
+    marker_offset_top,
+    marker_offset_bottom,
+    marker_size,
+    margin,
+    font_size,
+    divisor,
+    debug,
+    name,
+    transpose_upper,
+    transpose_lower,
+    page_width,
+    page_height,
+):
+
+    create_staves(
+        filename,
+        output,
+        musicbox,
+        marker_offset,
+        marker_offset_top,
+        marker_offset_bottom,
+        marker_size,
+        margin,
+        font_size,
+        divisor,
+        debug,
+        name,
+        transpose_upper,
+        transpose_lower,
+        page_width,
+        page_height,
+    )
+
+
+if __name__ == "__main__":
     main()
